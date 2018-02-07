36c145d5d5b459d3533bcf85344a5af923277b3a
FlipClown <sourmento@gmail.com>
2018-02-07 04:38:47 -0500

    Add files via upload

---
+++ news_fit_to_scrape/controllers/note.js
@@ -0,0 +1,30 @@
+// Controller for our notes
+// ========================
+var db = require("../models");
+
+module.exports = {
+    
+    findOne: function (req, res) {
+        db.Note
+            .findOne(req.query)
+            .then(function (dbNote) {
+                res.json(dbNote);
+            });
+    },
+   
+    create: function (req, res) {
+        db.Note
+            .create(req.body)
+            .then(function (dbNote) {
+                res.json(dbNote);
+            });
+    },
+   
+    delete: function (req, res) {
+        db.Note
+            .remove({ _id: req.params.id })
+            .then(function (dbNote) {
+                res.json(dbNote);
+            });
+    }
+};
